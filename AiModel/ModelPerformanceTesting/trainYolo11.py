ce53d83dffc3144df8baf790a98bd4bb24bbaef9
Tomas <tomassmitas@yahoo.com>
2025-07-27 11:19:25 +0100

    code added to train yolo basic model

--- AiModel/ModelPerformanceTesting/trainYolo11.py
+++ AiModel/ModelPerformanceTesting/trainYolo11.py
@@ -1,1 +1,23 @@
-print("Hello, world!")
+from ultralytics import YOLO
+
+# Load a COCO-pretrained YOLO11n model
+model = YOLO("yolo11n.pt")
+
+# Train the model on the COCO8 example dataset for 100 epochs
+results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+
+
+
+
+
+
+
+# @software{yolo11_ultralytics,
+#   author = {Glenn Jocher and Jing Qiu},
+#   title = {Ultralytics YOLO11},
+#   version = {11.0.0},
+#   year = {2024},
+#   url = {https://github.com/ultralytics/ultralytics},
+#   orcid = {0000-0001-5950-6979, 0000-0003-3783-7069},
+#   license = {AGPL-3.0}
+# }
